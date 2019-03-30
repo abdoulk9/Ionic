95db3ac353a0159a0122e8d568ba0afb0bf3b406
abdoulk9 <abdoulkadre@yahoo.fr>
2019-03-31 00:20:32 +0100

    projet initial

---
+++ ionic-todo-list/src/app/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { TodoService } from '../services/todo.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private todoService: TodoService){
+    console.log(todoService.getData());
+  }
+}
