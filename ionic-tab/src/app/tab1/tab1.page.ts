e4f92e00e8e827a00a85ef0d14bbad0eb7ab1ecd
abdoulk9 <abdoulkadre@yahoo.fr>
2019-04-03 10:22:25 +0200

    mise en place du http

--- ionic-tab/src/app/tab1/tab1.page.ts
+++ ionic-tab/src/app/tab1/tab1.page.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+
+  //injection du http
+  constructor(private http: HttpClient){
+    //creation d'une variable pour contenir une etablir une connection
+  let url= "https://randomuser.me/api?results=20";
+
+  //retour un observable
+  let  req = this.http.get(url);
+  //console.log(req);
+  req.subscribe(
+    (data)=> {
+      console.log(data);
+    }
+  )
+  }
+}
